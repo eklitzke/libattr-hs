65cbacc6bef25e42e3b57145a629f221be98918b
Evan Klitzke <evan@eklitzke.org>
2009-05-10 19:25:21 -0700

    use alloca, instead of malloc/free

--- Xattr.hs
+++ Xattr.hs
@@ -2,7 +2,12 @@
 {-# INCLUDE <attr/xattr.h> #-}
 {-# LANGUAGE ForeignFunctionInterface #-}
 
-module Xattr where
+module Xattr
+    ( setxattr
+    , getxattr
+    )
+    where
+
 import Foreign.C
 import Foreign.Ptr
 import Foreign.Marshal.Alloc
@@ -13,14 +18,17 @@ type Void = CChar
 allocBufSize :: Int
 allocBufSize = 1024
 
+allocCSize :: CSize
+allocCSize = fromIntegral allocBufSize
+
 foreign import ccall unsafe "setxattr" c_setxattr :: CString -> CString -> Ptr Void -> CSize -> CInt -> IO CInt
 
 setxattr :: String -> String -> BS.ByteString -> Int -> IO Int
 setxattr path name attrData flags = do
   cPath <- newCString path
   cName <- newCString name
-  val <- BS.useAsCStringLen attrData $ \(binaryData, dataLen) -> do
-      c_setxattr cPath cName binaryData (fromIntegral dataLen) (fromIntegral flags)
+  val <- BS.useAsCStringLen attrData $ \(binaryData, dataLen) ->
+                                    c_setxattr cPath cName binaryData (fromIntegral dataLen) (fromIntegral flags)
   return $ fromIntegral val
 
 foreign import ccall unsafe "getxattr" c_getxattr :: CString -> CString -> Ptr Void -> CSize -> IO CSize
@@ -29,8 +37,6 @@ getxattr :: String -> String -> IO BS.ByteString
 getxattr path name = do
   cPath <- newCString path
   cName <- newCString name
-  bytes <- mallocBytes allocBufSize
-  buflen <- c_getxattr cPath cName bytes (fromIntegral allocBufSize)
-  retval <- BS.packCStringLen (bytes, fromIntegral buflen)
-  free bytes
-  return retval
+  allocaBytes allocBufSize $ \mem -> do
+                       buflen <- c_getxattr cPath cName mem allocCSize
+                       BS.packCStringLen (mem, fromIntegral buflen)
